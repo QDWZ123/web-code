523e7664720bf10629dc2b0908b22bc60a19f49d
wangzhen <wangzhen@163.com>
2019-10-14 22:59:43 +0800

    three commit

--- TestGit.java
+++ TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("one commit");
 		System.out.println("two modify");
+		System.out.println("three modify");
 	}
 }
